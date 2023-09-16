51296916485e3ccd410518e640b85725236d9c30
shoaibshah110 <79579131+shoaibshah110@users.noreply.github.com>
2023-09-16 06:10:32 +0100

    started course

--- Functions,Variables.py
+++ Functions,Variables.py
@@ -1,8 +1,15 @@
 # Ask user for name
-name = input("whats your name? ")
+#name = input("whats your name? ")
 
 # Say hello to the user
-print("hello,")
-print(name)
+#print("hello,", name, "how are you doing today?")
+#print(name)
 
-# left off at 24 minutes
+#################################################
+
+favouriteCar = input("what is your favourite car? ")
+
+print(favouriteCar, "is a good choice")
+
+#need to watch from 40 minutes onwards
+
